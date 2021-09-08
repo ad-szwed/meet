cba3a296a5a55d8de543834cf6a45be340664c6b
ad-szwed <shvedatech@gmail.com>
2021-09-08 18:00:20 +0100

    4.4 integration 1st test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
-import CitySearch from './CitySearch'
+import CitySearch from './CitySearch';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: []
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch />
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList'
 import CitySearch from '../CitySearch'
 
+// UNIT TESTING WITH SHALLOW RENDERING:
+
+// describe() describes the scope of testing
 describe('<App /> component', () => {
   let AppWrapper;
   beforeAll(() => {
@@ -16,5 +19,16 @@ describe('<App /> component', () => {
 
   test('render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+});
+
+// INTEGRATION TESTING WITH FULL RENDERING (not required to split, but a best practice!)
+describe('<App /> integration', () => {
+  test('App passes "events" state as a prop to EventList', () => {
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state('events');
+    expect(AppEventsState).not.toEqual(undefined);
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    AppWrapper.unmount();
   });
 });
