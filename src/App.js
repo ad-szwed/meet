3c37b21a53d6c009c9a0ecc38cfa74d69cab0a32
ad-szwed <shvedatech@gmail.com>
2021-09-10 16:42:16 +0100

    4.4 UI update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import { getEvents } from './api';
+import { getEvents, extractLocations } from './api';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
   state = {
     events: [],
     locations: []
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
   }
 
   updateEvents = (location) => {
@@ -24,8 +38,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
+        <CitySearch
+          locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList
+          events={this.state.events} />
+        <NumberOfEvents
+          numberOfEvents={this.state.numberOfEvents}
+          updateEvents={this.updateEvents} />
       </div>
     );
   }
