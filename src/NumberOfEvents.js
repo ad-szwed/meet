b6b82cad71925bae9d8d154b707c8c0c2b160cda
ad-szwed <shvedatech@gmail.com>
2021-09-30 10:05:41 +0100

    Revert "4.7 task - info and error alerts implemented"
    This reverts commit afe9b1b265982c88a99c25b49667c7a79a227709.

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
@@ -11,20 +10,13 @@ class NumberOfEvents extends Component {
     const eventCount = event.target.value;
     if (eventCount < 1) {
       return this.setState({
-        eventCount: "",
-        errorText: 'Please select a number between 1 and 32',
-      });
-    } else if (eventCount > 32) {
-      return this.setState({
-        eventCount: "",
-        errorText: 'Please select a number between 1 and 32',
+        eventCount: 32
       });
     } else {
       this.setState({
-        eventCount,
-        errorText: "",
+        eventCount
       });
-      this.props.updateEvents("", eventCount);
+      this.props.updateEvents('', eventCount);
     }
   };
 
@@ -39,7 +31,6 @@ class NumberOfEvents extends Component {
           value={this.state.eventCount}
           onChange={this.handleEventInputChanged}
         />
-        <ErrorAlert text={this.state.errorText} />
       </div>
     );
   }
