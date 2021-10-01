9ea0c1836ca1b72c6fcf40e66bc8b9f1e94c85ed
ad-szwed <shvedatech@gmail.com>
2021-10-01 11:40:53 +0100

    fixing errors

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,58 +1,93 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { InfoAlert } from './Alert';
-class CitySearch extends Component {
-  state = {
-    query: '',
-    suggestions: [],
-    showSuggestions: false,
+
+class CitySearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      suggestions: [],
+      showSuggestions: false,
+    };
   }
+
+  // update state of city in text input
   handleInputChanged = (event) => {
-    const value = event.target.value;
-    this.setState({showSuggestions:true});
-    const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    });
+    const { value } = event.target;
+    const { locations } = this.props;
+    const suggestions = locations.filter(
+      (location) => location.toUpperCase().indexOf(value.toUpperCase()) > -1,
+    );
     if (suggestions.length === 0) {
       this.setState({
         query: value,
-        infoText: 'We can not find the city you are looking for. Please try another city',
+        infoText: 'We can not find the city you are looking for. Please try again',
       });
     } else {
-      return this.setState({
+      this.setState({
         query: value,
         suggestions,
-        showSuggestions: false,
-        infoText: ''
+        infoText: '',
       });
     }
   };
-  
-  handleItemClicked = (suggestion) => {
+
+  handleItemClicked(suggestion) {
     this.setState({
       query: suggestion,
-      showSuggestions: false
+      // suggestions: [],
+      showSuggestions: false,
+      infoText: '',
     });
-    this.props.updateEvents(suggestion);
+    this.props.updateEvents(suggestion, this.props.numberOfEvents);
   }
+
+  listUpdate() {
+    const { locations } = this.props;
+    const suggestions = locations.filter((location) => location);
+
+    this.setState({
+      suggestions,
+    });
+  }
+
   render() {
+    const { query, showSuggestions, suggestions } = this.state;
     return (
       <div className="CitySearch">
+        <p> </p>
+        <label htmlFor="CitySearch">Select a City:</label>
         <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
-          value={this.state.query}
-          onChange={this.handleInputChanged}
-          onFocus={() => { this.setState({ showSuggestions: true }) }}
+          value={query}
+          onChange={(e) => this.handleInputChanged(e)}
+          placeholder="enter city here"
+          onFocus={() => {
+            this.setState({ showSuggestions: true });
+          }}
+          onClick={() => {
+            this.setState({ showSuggestions: true });
+          }}
         />
-        <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
-          {this.state.suggestions.map((suggestion) => (
-            <li
-              key={suggestion}
-              onClick={() => this.handleItemClicked(suggestion)}
-            >{suggestion}</li>
-          ))}
-          <li onClick={() => this.handleItemClicked("all")}>
+        <ul className="suggestions" style={showSuggestions ? {} : { display: 'none' }}>
+          {(suggestions.length >= 1)
+            ? suggestions.map((suggestion) => (
+              <li
+                className="matchSuggestions"
+                key={suggestion}
+                onClick={() => this.handleItemClicked(suggestion)}
+              >
+                {suggestion}
+              </li>
+            ))
+            : null}
+          <li
+            className="matchSuggestions"
+            key="all"
+            onClick={() => this.handleItemClicked('all')}
+          >
             <b>See all cities</b>
           </li>
         </ul>
@@ -60,4 +95,5 @@ class CitySearch extends Component {
     );
   }
 }
-export default CitySearch
+
+export default CitySearch;
