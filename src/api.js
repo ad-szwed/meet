2bb4577f74b97766d369583beb45007a7a561013
ad-szwed <shvedatech@gmail.com>
2021-10-05 14:50:49 +0100

    4.8 offline capability

--- src/api.js
+++ src/api.js
@@ -58,6 +58,14 @@ const checkToken = async (accessToken) => {
 export const getEvents = async () => {
   NProgress.start();
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return {
+      events: JSON.parse(events).events,
+      locations: extractLocations(JSON.parse(events).events),
+    };
+
   if (window.location.href.startsWith("http://localhost")) {
     NProgress.done();
     return mockData;
